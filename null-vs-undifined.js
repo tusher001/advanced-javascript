eef0ff2bf52ac6c4834e37fd86a9d2f1e7dc4660
tusher001 <tusherd3@gmail.com>
2021-05-04 22:46:42 +0600

    null vs undifind complete

---
+++ null-vs-undifined.js
@@ -0,0 +1,10 @@
+// let something;
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return
+// }
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(44);
+console.log(result);
